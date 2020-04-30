ef379e38736b012971380a214fae4ce20adf2fb9
justincase9 <justas.birsotne@gmail.com>
2020-04-30 15:56:40 +0300

    Added force arrows

--- AR_EDU/Assets/Scripts/cannon.cs
+++ AR_EDU/Assets/Scripts/cannon.cs
@@ -11,13 +11,19 @@ public class cannon : MonoBehaviour
     public static float globalGravity = -9.81f;
     bool animating = true;
 
+
+    LineRenderer lr;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponentInChildren<Rigidbody>();
         rb.useGravity = false;
+        //this.GetComponentInChildren<Rigidbody>().AddForce(GameObject.Find("cannon").transform.up * force);
+        //rb.gameObject.transform.Rotate(transform.rotation.eulerAngles - Quaternion.Euler(0, 90, 0).eulerAngles);
         this.GetComponentInChildren<Rigidbody>().AddForce(rb.transform.up * force);
+        lr = this.GetComponentInChildren<LineRenderer>();
+        this.GetComponentInChildren<LineRenderer>().positionCount = 6;
     }
 
     // Update is called once per frame
@@ -35,10 +41,14 @@ public class cannon : MonoBehaviour
                 animating = false;
                 //rb.gameObject.GetComponent<LineRenderer>().enabled = false;
             }
-            
-            //Vector2 v = rb.velocity;
-            //float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
-            //rb.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+            lr.SetPosition(0, rb.transform.position);
+            lr.SetPosition(1, rb.transform.position + (rb.velocity * 0.15f));
+            lr.SetPosition(2, rb.transform.position + (rb.velocity * 0.175f));
+            lr.SetPosition(3, rb.transform.position);
+            lr.SetPosition(4, rb.transform.position + (new Vector3(0, -.8f, 0) * 0.15f));
+            lr.SetPosition(5, rb.transform.position + (new Vector3(0, -.8f, 0) * 0.175f));
+            lr.widthCurve = new AnimationCurve(new Keyframe(0, 0.8f), new Keyframe(0.2f, .8f), new Keyframe(.4f, 0), new Keyframe(.531f, 0.8f), new Keyframe(.9f, 0.8f), new Keyframe(1f, 0f));
         }
     }
 }
